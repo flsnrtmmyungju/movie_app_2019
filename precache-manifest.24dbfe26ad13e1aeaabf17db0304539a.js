5adf93bbb720959da51da31890cd4b44986f7587
flsnrtmmyungju <flsnrtmmyungju@naver.com>
2019-11-01 13:25:58 +0900

    Updates

---
+++ precache-manifest.24dbfe26ad13e1aeaabf17db0304539a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95313ada7bc423951c5977f221f61bf8",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "01e6abffbd06384cb9f4",
+    "url": "/movie_app_2019/static/css/main.888d1aad.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "01e6abffbd06384cb9f4",
+    "url": "/movie_app_2019/static/js/main.0e5940e2.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
